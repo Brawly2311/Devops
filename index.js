2ad3af0412b99fbde6cefd6f7b113e63d91fa7c8
Brawly2311 <tu_email@ejemplo.com>
2025-12-14 10:04:04 -0400

    Proyecto Hola Mundo DevOps

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo DevOps');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App corriendo en el puerto ${port}`);
+});
+
+module.exports = app;
---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+﻿const request = require('supertest');
+const app = require('../index');
+
+test('Debe responder Hola Mundo DevOps', async () => {
+  const response = await request(app).get('/');
+  expect(response.text).toBe('Hola Mundo DevOps');
+});
